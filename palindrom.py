97da611b81b4cd8044253bb774e95fa3d83b62b6
Dimid19 <dimapotapenko432@gmail.com>
2023-09-11 02:57:35 +0000

    check results

--- palindrom.py
+++ palindrom.py
@@ -7,4 +7,9 @@ def get_longest_palindrome(origin: str) -> str:
             if substring == substring[::-1] and len(substring) > len(longest_palindrome):
                 longest_palindrome = substring
     return longest_palindrome
-
+input_str1 = "0123219"
+input_str2 = "1012210"
+result1 = get_longest_palindrome(input_str1)
+result2 = get_longest_palindrome(input_str2)
+print("The longest palindrom for '0123219':", result1)
+print("THe longest palindrom for  '1012210':", result2
