f91cb189737ea0db15c78245468acf9fbda30976
Dimid19 <dimapotapenko432@gmail.com>
2023-09-11 02:56:22 +0000

    We check which substring is a palindrome and which one was found

--- palindrom.py
+++ palindrom.py
@@ -4,4 +4,7 @@ def get_longest_palindrome(origin: str) -> str:
     for i in range(len(origin)):
         for j in range(i + 1, len(origin) + 1):
             substring = origin[i:j]
-        
+            if substring == substring[::-1] and len(substring) > len(longest_palindrome):
+                longest_palindrome = substring
+    return longest_palindrome
+
