2dc8ba57dac33fd841db45cfdd82afb126e9816f
Dimid19 <dimapotapenko432@gmail.com>
2023-09-11 02:41:00 +0000

    convert to string

--- palindrom.py
+++ palindrom.py
@@ -1,1 +1,5 @@
-Get Started
+from typing import Union
+user_input = input("Add string and number: ")
+def is_palindrome(origin: Union[str, int]) -> bool:
+    origin = str(origin)
+    length = len(origin)
