c749dec6b3b898af08007d4dd0bc58f3e3c4b121
Dimid19 <dimapotapenko432@gmail.com>
2023-09-11 02:42:42 +0000

    Checking with a loop

--- palindrom.py
+++ palindrom.py
@@ -3,3 +3,7 @@ user_input = input("Add string and number: ")
 def is_palindrome(origin: Union[str, int]) -> bool:
     origin = str(origin)
     length = len(origin)
+    for i in range(length // 2):
+        if origin[i] != origin[length - 1 - i]:
+            return False
+    return  True
