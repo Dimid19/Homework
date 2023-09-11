30c36a2320dfed6dde1a5494b4d139f9a074b1c8
Dimid19 <dimapotapenko432@gmail.com>
2023-09-11 02:54:51 +0000

    Create two loops

--- palindrom.py
+++ palindrom.py
@@ -1,1 +1,7 @@
-Get Started
+def get_longest_palindrome(origin: str) -> str:
+    longest_palindrome = ""
+
+    for i in range(len(origin)):
+        for j in range(i + 1, len(origin) + 1):
+            substring = origin[i:j]
+        
