bcbe7ab19098e5677b9e271e9ed175c37404d79a
Dimid19 <dimapotapenko432@gmail.com>
2023-09-18 12:28:38 +0000

    Insert into table data

--- Createtable.sql
+++ Createtable.sql
@@ -29,3 +29,36 @@ Create table movie_people
     director          VARCHAR(255) NOT NULL,
     screenwriter      VARCHAR(255) NOT NULL
 );
+INSERT INTO genre (name) VALUES
+    ('Fantasy'),
+    ('War'),
+    ('Detective'),
+    ('Horror'),
+    ('Comedy'),
+    ('Drama');
+INSERT INTO movie (title, tagline, summary, release_year, genre_id)
+VALUES
+    ('Lord of the Rings','My precission','A journey to save Middle-earth', 2001,1),
+    ('The hobbit','Smoge','Bilbo Baggins joins an adventure', 2012,1),
+    ('Fury','Is the best joob','Tank crew mission', 2014,2),
+    ('Sherloc','ELementarno','Modern-day Sherlock Holmes',2010,3),
+    ('Mirors','Don`t see','Supernatural events in a mirror', 2008,4),
+    ('Macho and Botan','Anti kisiak','Hilarious misadventures', 2007,5),
+    ('Hatiko','Forewer love','Loyal dog tale of devotion', 2009,6);
+INSERT INTO people (name, birth_day)
+VALUES
+     ('Cristofer Li','1990-01-15'),
+     ('Bread Pitt','1985-03-20'),
+     ('cumberbatch','1985-03-20'),
+     ('Emi Smart','1970-07-10'),
+     ('Jonnie Depp','1980-05-25'),
+     ('Hachiko owner','1988-12-05');
+INSERT INTO  movie_people (movie_id, people_id, name_personage, director, screenwriter)
+VALUES
+    (1,1,'Gendalf grey','Director1','screenwriter1'),
+    (2,1, 'Gendalf grey','Director2','screenwriter2'),
+    (3,2,'Don Kolnerr','Director3','screenwriter3'),
+    (4,3,'Doktor Watsonn','Director4','screenwriter4'),
+    (5,4,'Mary','Director5','screenwriter5'),
+    (6,5,'Tom Henson','Director6','screenwriter6'),
+    (7,6,'THe dog Hatiko','Director7','screenwriter7');
